0f684633fcb34339ca9970af3a41198164e169cc
onnymm <phr2807@gmail.com>
2026-06-12 16:38:49 -0700

    Añadido error HTTPS de UUID de sesión inválida en obtención de los datos del usuario de la sesión

--- app/_routes/account.py
+++ app/_routes/account.py
@@ -2,12 +2,14 @@ from typing import Annotated
 from fastapi import status
 from fastapi import APIRouter
 from fastapi import Depends
+from lylac.errors import InvalidSessionUUIDError
 from .._constants import ENDPOINT_NAME
 from .._constants import ENDPOINT_PATH
 from .._constants import PRESET_PROFILE_FIELDS
 from .._constants import ROUTER_PREFIX
 from .._constants import TAG
 from .._core import iacele
+from .._errors import invalid_token_error
 from .._security import authenticate_user
 
 # Inicialización de router de transacciones de cuenta
@@ -25,10 +27,15 @@ async def _me(
     session_uuid: Annotated[str, Depends(authenticate_user)],
 ) -> dict:
 
-    # Obtención de los datos del usuario
-    my_user_data = iacele.me(
-        session_uuid,
-        PRESET_PROFILE_FIELDS,
-    )
+    # Intento de obtención de los datos de usuario
+    try:
+        my_user_data = iacele.me(
+            session_uuid,
+            PRESET_PROFILE_FIELDS,
+        )
+    # Si la UUID de sesión es inválida...
+    except InvalidSessionUUIDError:
+        # Se arroja error
+        raise invalid_token_error
 
     return my_user_data
