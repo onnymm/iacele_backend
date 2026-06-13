8f54bb49e9a6213702bbe8772deeff25cb341c53
onnymm <phr2807@gmail.com>
2026-06-13 13:38:38 -0700

    Ajustes en tipados base de modelos de FastAPI

--- app/_api/models/__init__.py
+++ app/_api/models/__init__.py
@@ -1,4 +1,5 @@
 from ._authentication import TokenData
 from ._crud import CRUD
 from ._metadata import Metadata
+from ._frontend import Frontend
 from ._server import Server
--- app/_api/models/_attributes.py
+++ app/_api/models/_attributes.py
@@ -1,9 +1,33 @@
 from typing import Optional
+from typing import TypeVar
 from pydantic import BaseModel
 from pydantic import BaseModel
 from lylac import CriteriaStructure
 
-_FieldAlias = tuple[str, str]
+_T = TypeVar('_T')
+
+_ScalarOrList = _T | list[_T]
+
+_FieldName = str
+
+ExpandedRecords = tuple[_FieldName | _T]
+
+_AliasedField = tuple[_T, str]
+
+_SupportsAlias = _T | _AliasedField[_T]
+
+_FlatOrNested = _SupportsAlias[_FieldName] | _SupportsAlias[ExpandedRecords[_T]]
+
+FieldsSelectionLevel4 = list[_SupportsAlias[_FieldName]]
+
+FieldsSelectionLevel3 = list[_FlatOrNested[FieldsSelectionLevel4]]
+
+FieldsSelectionLevel2 = list[_FlatOrNested[FieldsSelectionLevel3]]
+
+FieldsSelectionLevel1 = list[_FlatOrNested[FieldsSelectionLevel2]]
+
+class _SupportsSelectableFieldsLevel_3:
+    fields: FieldsSelectionLevel3
 
 class _RequiresModelName(BaseModel):
     model_name: str
@@ -28,11 +52,14 @@ class _RequiresRecordID(BaseModel):
     record_id: int
 
 class _RequiresRecordIDs(BaseModel):
-    record_ids: int | list[int]
+    record_ids: _ScalarOrList[int]
+
+class _RequiresRecordID(BaseModel):
+    record_ids: int
 
 class _SupportsSelectableFields(BaseModel):
-    fields: list[str | _FieldAlias] = []
+    fields: FieldsSelectionLevel1 = []
 
 class _SupportsSorting(BaseModel):
-    sortby: Optional[str | list[str]] = None
-    ascending: Optional[bool | list[bool]] = None
+    sortby: Optional[_ScalarOrList[str]] = None
+    ascending: Optional[_ScalarOrList[bool]] = None
